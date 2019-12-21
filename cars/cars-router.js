bc530a52db561006970bda6d8121b1cf7df3b501
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-12-21 11:50:38 -0700

    MVP met endpoints for create and read work

---
+++ cars/cars-router.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const db = require("../db");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const cars = await db("cars").select();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:VIN", async (req, res, next) => {
+  try {
+    const cars = await db("cars")
+      .where({ VIN: req.params.VIN })
+      .first();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const vin = await db("cars").insert(req.body);
+    const newCar = await db("cars")
+      .where({ VIN: vin[0] })
+      .first();
+    res.status(201).json(newCar);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
+const carRouter = require("./cars/cars-router")
 const server = express();
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 5000;
-
 server.use(express.json());
 
-// server.use("/api/cars");
+server.use("/api/cars", carRouter);
 server.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({
